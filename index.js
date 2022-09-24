686c206f5421643beedfdb698f5d149893867e8c
Nishant <nishant.kumar@sheshi.ai>
2022-09-25 01:39:49 +0530

    fix: dynamic port '

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 const db = require("./config/mongoose");
 app.use("/api", require("./routes/api"));
 
-app.listen(3000, () => {
+app.listen(process.env.PORT||3000, () => {
   console.log(`Server started on port ${3000}`);
 });
 
